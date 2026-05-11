4298dcff7061c1b0f400e4f9c531385a38b57359
Magnus Egeland <magnus.egeland@gmail.com>
2026-05-11 12:01:22 +0000

    update watermark size

--- src/bft-smart.gorums/server/log.go
+++ src/bft-smart.gorums/server/log.go
@@ -150,8 +150,8 @@ func (c *CheckpointState) recordVote(replicaID uint32, digest []byte, msg *pb.Ch
 }
 
 const (
-	CheckpointInterval = 18000
-	WaterMarkWindow    = 36000
+	CheckpointInterval = 100000
+	WaterMarkWindow    = 200000
 )
 
 // MessageLog is the central state store for in-flight consensus entries and checkpoints.
